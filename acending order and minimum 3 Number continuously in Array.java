1ebf1687dd1e73a384990c39fc562f351fcb6b21
HarshilShiyani <95012680+HarshilShiyani@users.noreply.github.com>
2022-06-28 19:05:50 +0530

    Update acending order and minimum 3 Number continuously in Array.java

--- acending order and minimum 3 Number continuously in Array.java
+++ acending order and minimum 3 Number continuously in Array.java
@@ -6,10 +6,11 @@ class Array_min3_Asc_Order_in_Array{
 	public static void main(String[] args) {
 
 		Scanner sc =new Scanner(System.in);
-		int arr[]=new int[]{1,1,1,1,1,2,2,2,3,4,4,4};
+		int arr[]=new int[]{1,1,1,2,2,2,2,3,3,3,4};
 		int count=0;
 		int min_value=1;
-		for(int i=1;i<arr.length;i++)
+		int i=1;
+		while(i<arr.length)
 		{
 			if(arr[i-1]<=arr[i])
 			{
@@ -30,21 +31,27 @@ class Array_min3_Asc_Order_in_Array{
 				{
 					break;
 				}*/
-				
+			i++;	
 			}
+		
 			else
 			{
 				count++;
 				break;
 			}
 		}
-		if(count==0)
+		if(min_value<3)
+		{
+			System.out.print(0);
+		}
+		else if(count==0)
 		{
 			System.out.print(1);
 		}
+		
 		else
 		{
-			System.out.print(0);
+			System.out.println(0);
 		}
 	}
 }
