bd4ffa1706668cfa0b7e1bcf5d35469300606dfa
HarshilShiyani <95012680+HarshilShiyani@users.noreply.github.com>
2022-09-27 19:00:25 +0530

    Add files via upload

--- BstDemo.java
+++ BstDemo.java
@@ -1,118 +1,112 @@
 import java.util.Stack;
-class Bst{
 
-	Node root;
-	class Node{
-		Node left;
-		Node right;
-		int data;
+class Bst {
 
-	 Node(int data){
-	 	this.data=data;
-	 	this.left=null;
-	 	this.right=null;
+    Node root = null;
 
-	 }
-	}
+    class Node {
+        Node left;
+        Node right;
+        int data;
 
-	void insert(int d){
-	Node newnode=new Node(d);
-	if(root==null){
-		root=newnode;
-	}
-	else
-	{
-	Node temp=root;
-		
-	while(root.left!=null||root.right!=null){
-		if(newnode.data>=temp.data&&temp.right!=null)
-		{
-			temp=temp.right;
-		}
-		else if(newnode.data<temp.data&&temp.left!=null)
-		{
-			temp=temp.left;
-		}
-		else
-		{
-			break;
-		}
-	}
-		if(newnode.data>=temp.data){
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
 
-			temp.right=newnode;
-		}
-		else{
+        }
+    }
 
-			temp.left=newnode;
-		}
+    /////
+    void inorder() {
+        inorderRec(root);
+    }
 
-		}
-	}
-	void display_inorder()
-	{	
-		Stack<Node> stk=new Stack<Node>();
-		
-		Node temp=root;
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " -> ");
+            inorderRec(root.right);
+        }
+    }
 
-			while(temp!=null||stk.size()>0)
-			{
-				while(temp!=null)
-				{
-					stk.push(temp);					
-					temp=temp.left;
+    ///////
+    void preorder() {
+        preorderRec(root);
+    }
 
-				} 
-					temp=stk.pop();
-					System.out.println(temp.data);
-					temp=temp.right;
-				
-			}
-	}
-	void display_preorder(){
-		if(root==null)
-		{
-			System.out.print("null--");
-		}
-		else
-		{
-			Stack<Node> stk1=new Stack<Node>();
-			Node demo=root;
-			stk1.push(demo);
-			while(stk1.size()>0)
-			{
-				demo=stk1.pop();
-				System.out.println(demo.data);
-			
-			if(demo.right!=null)
-			{
-				stk1.push(demo.right);
-			}
-			else if(demo.left!=null)
-			{
-				stk1.push(demo.left);
-			}
+    void preorderRec(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " -> ");
+            preorderRec(root.left);
 
-		}
-	}
-	}
+            preorderRec(root.right);
+        }
+    }
 
+    ///////
+    void postorder() {
+        postorderRec(root);
+    }
+
+    void postorderRec(Node root) {
+
+        if (root != null) {
+            postorderRec(root.left);
+            postorderRec(root.right);
+            System.out.print(root.data + " -> ");
+        }
+    }
+
+    /////
+    void insert(int d) {
+        Node newnode = new Node(d);
+        if (root == null) {
+            root = newnode;
+        } else {
+            Node temp = root;
+
+            while (root.left != null || root.right != null) {
+                if (newnode.data >= temp.data && temp.right != null) {
+                    temp = temp.right;
+                } else if (newnode.data < temp.data && temp.left != null) {
+                    temp = temp.left;
+                } else {
+                    break;
+                }
+            }
+            if (newnode.data >= temp.data) {
+
+                temp.right = newnode;
+            } else {
+
+                temp.left = newnode;
+            }
+
+        }
+    }
+    void delete(){
+        
+    }
 }
-class BstDemo
-{
 
-	public static void main(String[] args) {
+class BstDemo {
 
-		Bst obj=new Bst();
-		obj.insert(9);
-		obj.insert(15);
-		obj.insert(18);
-		obj.insert(1);
-		obj.insert(104);
-		obj.insert(166);
-		obj.display_inorder();
-		System.out.println("===========");
-		obj.display_preorder();
-		
-	}
+    public static void main(String[] args) {
+
+        Bst obj = new Bst();
+        obj.insert(10);
+        obj.insert(0);
+        obj.insert(20);
+        obj.insert(50);
+        obj.insert(1);
+
+        obj.inorder();
+        obj.inorder();
+        System.out.println("");
+        obj.preorder();
+        System.out.println("");
+        obj.postorder();
+
+    }
 }
--- DoubleLink.java
+++ DoubleLink.java
@@ -1,69 +1,60 @@
-class DoubleLinkedList{
+class DoubleLink{
 
-	
-	class Node{
+	class Node {
 
 		int data;
 		Node next;
 		Node prev;
 
-		Node(int data)
-		{
-			this.data=data;
-			this.next=null;
-			this.prev=null;
-		}
-	}
-	public Node head=null;
-	public Node last=null;
-	Node Last;
-	void insert_in_doubly(int data){
-
-		Node newnode=new Node(data);
-		if(head==null)
-		{
-			head=newnode;
-			last=newnode;
-		}
-		else
-		{
-			last.next=newnode;
-			newnode.prev=last;
-			last=newnode;
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+			this.prev = null;
 		}
 	}
 
-	void displaynode(){
-		Node current=head;
-		if(head==null)
-		{
+	public Node head = null;
+	public Node last = null;
+
+	void insert_in_doubly(int data) {
+
+		Node newnode = new Node(data);
+		if (head == null) {
+			head = newnode;
+			last = newnode;
+		} else {
+			last.next = newnode;
+			newnode.prev = last;
+			last = newnode;
+		}
+	}
+
+	void displaynode() {
+		Node current = head;
+		if (head == null) {
 			System.out.print("Empty");
 
-		}
-		else
-		{
-			while(current!=null)
-			{
-				System.out.print(current.data+" ");
-				current=current.next;
+		} else {
+			while (current != null) {
+				System.out.print(current.data + " ");
+				current = current.next;
 			}
 		}
 	}
 
 }
-class DoubleLink{
 
-	public static void main(String[] args) 
-	{
+class DoubleLinkDemo {
+	public static void main(String[] args) {
 
-		DoubleLinkedList obj=new DoubleLinkedList();
+		DoubleLink obj = new DoubleLink();
 		obj.insert_in_doubly(10);
 		obj.insert_in_doubly(20);
 		obj.insert_in_doubly(30);
 		obj.insert_in_doubly(40);
-		obj.insert_in_doubly(80kgkfjgfgkfjgkfjgkfjg);
+		obj.insert_in_doubly(80);
 		obj.displaynode();
-		System.out.print(" Prev="+obj.last.prev.prev.next.prev.prev.data);
-		
+		//System.out.print(" Prev=" + obj.last.prev.prev.next.prev.prev.data);
+
 	}
 }
