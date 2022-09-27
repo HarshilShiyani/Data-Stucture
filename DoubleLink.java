cacd17f1be03016cc6693650632ad0d8ef1e2905
HarshilShiyani <95012680+HarshilShiyani@users.noreply.github.com>
2022-09-27 23:03:38 +0530

    Update DoubleLink.java

--- DoubleLink.java
+++ DoubleLink.java
@@ -1,4 +1,4 @@
-class DoubleLink{
+class DoubleLink {
 
 	class Node {
 
@@ -29,6 +29,17 @@ class DoubleLink{
 		}
 	}
 
+	void inser_first(int data) {
+		Node newnode = new Node(data);
+		if (head == null) {
+			head = newnode;
+			last = newnode;
+		} else {
+			newnode.next = head;
+			head = newnode;
+		}
+	}
+
 	void displaynode() {
 		Node current = head;
 		if (head == null) {
@@ -42,6 +53,34 @@ class DoubleLink{
 		}
 	}
 
+	void deleteNode(int value) {
+
+		Node temp = head;
+		if (temp == null) {
+			System.out.println("Empty that nor delete");
+		} else if (value == temp.data) {
+
+			temp = temp.next;
+			temp.prev = null;
+			displaynode();
+
+		} else if (value == temp.next.data) {
+			temp.prev.next=temp.next;
+			temp.next.prev = temp.prev;
+			displaynode();
+		}
+
+		else {
+			while (temp.data != value && temp != null) {
+				temp = temp.next;
+			}
+			temp.prev.next = temp.next;
+			temp.next = temp.prev.next;
+			temp = temp.next;
+			displaynode();
+		}
+
+	}
 }
 
 class DoubleLinkDemo {
@@ -53,8 +92,10 @@ class DoubleLinkDemo {
 		obj.insert_in_doubly(30);
 		obj.insert_in_doubly(40);
 		obj.insert_in_doubly(80);
-		obj.displaynode();
-		//System.out.print(" Prev=" + obj.last.prev.prev.next.prev.prev.data);
+		obj.inser_first(1);
 
+		obj.displaynode();
+		System.out.println("");
+		obj.deleteNode(20);
 	}
 }
