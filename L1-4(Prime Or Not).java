60312ce6329e063e579925d0a8d0a8c68acc729e
Vinay Dabasiya <77038630+Vnayd@users.noreply.github.com>
2022-11-18 15:09:57 +0530

    create isprime function

--- L1-4(Prime Or Not).java
+++ L1-4(Prime Or Not).java
@@ -1,28 +1,23 @@
 import java.util.*;
 class prime{
+	public static boolean isprime(int n) {
+		if(n < 2)
+			return false;
+		for(int i=2; i<=(int)Math.sqrt(n); i++) {
+			if(n%i == 0)
+				return false;
+		}
+		return true;
+	}
 	public static void main(String[] args) {
 
 		Scanner sc =new Scanner(System.in);
 		int n=sc.nextInt();
 		int flag=0;
-		for (int i=1;i<=n;i++) 
-		{
-			if(n%i==0)
-			{
-				flag++;
-				break;
-			}
-
-			
-		}
-		if(flag==0)
-		{
-			System.out.print("prime");
-		}
-		else
-		{
-			System.out.print(" not prime");
-		}
+		if(isprime(n))
+			System.out.println("prime");
+		else 
+			System.out.println("not prime");
 
 	}
 }
